d2144d23d605913500581e36384a85f42f04a559
DucVinh25 <151304889+DucVinh25@users.noreply.github.com>
2025-05-20 22:42:48 +0700

    sua su kien

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ require_once BASE_PATH . '/controllers/TicketController.php';
 require_once BASE_PATH . '/controllers/StaffController.php';
 require_once BASE_PATH . '/controllers/BookingController.php';
 require_once BASE_PATH . '/controllers/PointsController.php';
+require_once BASE_PATH . '/controllers/MomoPaymentController.php';
 
 
 // Lấy đường dẫn hiện tại
@@ -89,11 +90,6 @@ switch ($path) {
         $adminController = new AdminController();
         $adminController->pendingEvents();
         break;
-    
-    case '/admin/revenue':
-        $adminController = new AdminController();
-        $adminController->revenue();
-        break;
 
     case '/admin/approve-event':
         $adminController = new AdminController();
@@ -123,6 +119,11 @@ switch ($path) {
     case '/organizer/events/delete':
         $organizerEventController = new OrganizerEventController();
         $organizerEventController->delete($_GET['id']);
+        break;
+
+    case '/organizer/revenue':
+        $organizerEventController = new OrganizerEventController();
+        $organizerEventController->revenue();
         break;
 
     case '/account':
@@ -260,10 +261,26 @@ switch ($path) {
         $bookingController->processPayment();
         break;
 
-    case '/organizer/revenue':
-        $organizerEventController = new OrganizerEventController();
-        $organizerEventController->revenue();
+
+    case '/momo-payment/process':
+        $momoPaymentController = new MomoPaymentController();
+        $momoPaymentController->processPayment();
         break;
+
+    case '/momo-payment/ipn':
+        $momoPaymentController = new MomoPaymentController();
+        $momoPaymentController->ipn();
+        break;
+
+    case '/momo-payment/return':
+        $momoPaymentController = new MomoPaymentController();
+        $momoPaymentController->return();
+        break;
+
+    case '/momo-payment/thanks':
+        $momoPaymentController = new MomoPaymentController();
+        $momoPaymentController->thanks();
+
 
     default:
         // Kiểm tra xem có phải là route chi tiết sự kiện không
